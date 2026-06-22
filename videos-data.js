05d219aaff3162be5fcdf498c2e60688d8b26b1c
dadgumet <dadgumet@gmail.com>
2026-06-22 06:54:00 -0400

    Update videos-data.js

--- videos-data.js
+++ videos-data.js
@@ -150,12 +150,12 @@ const videosData = {
                 ]
             },
             'Obeying': {
-                emoji: '🧭',
+                emoji: '🫡',
                 episodes: 'Episodes 53-56',
                 bibleBooks: '1st Chronicles-2nd Chronicles',
                 videos: [
                     { id: 'obeying1', title: 'Obeying 1 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A27AObeying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A27AObeying_d.mp4' },
-                    // { id: 'obeying2', title: 'Obeying 2 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A27BObeying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A27BObeying_d.mp4' },
+                    { id: 'obeying2', title: 'Obeying 2 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A27BObeying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A27BObeying_d.mp4' },
                     // { id: 'obeying3', title: 'Obeying 3 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A28AObeying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A28AObeying_d.mp4' },
                     // { id: 'obeying4', title: 'Obeying 4 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A28BObeying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A28BObeying_d.mp4' }
                 ]
